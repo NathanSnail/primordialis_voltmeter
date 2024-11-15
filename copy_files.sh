34ea0dbb6b5a19a81ca344cfaa76d1e0b9c53694
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-11-16 00:58:19 +1300

    build process

---
+++ copy_files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp voltmeter.dll "~/.local/share/Steam/steamapps/common/Primordialis Demo/mods/voltmeter/"
+cp mod-info.txt "~/.local/share/Steam/steamapps/common/Primordialis Demo/mods/voltmeter/"
