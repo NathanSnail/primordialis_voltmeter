df7ae2d929ff5ec615c293f1a49f81024ec78608
Nathan <47441022+NathanSnail@users.noreply.github.com>
2024-11-16 13:15:41 +1300

    make it work with new loader setup

--- src/voltmeter.c
+++ src/voltmeter.c
@@ -11,23 +11,3 @@ static inline void *Address(size_t value) {
 
 // this function will likely be removed in later versions of the modloader
 __declspec(dllexport) void init() { MessageBoxA(NULL, "grr", "hia", MB_OK); }
-
-void testInjection() {
-	printf("test injection\n");
-
-	MessageBoxA(NULL, "test injection", "Voltmeter", MB_OK);
-}
-
-__declspec(dllexport) void detourInit(void (*DetourAttach)(void *, void *)) {
-	while (1) {}
-	freopen("CONOUT$", "w", stdout);
-	printf("detourInit voltmeter");
-	DetourAttach(NULL, NULL);
-
-	baseAddress = (size_t)GetModuleHandleA(NULL);
-	PVOID *func_ptr = (PVOID *)malloc(sizeof(PVOID));
-	void *TargetFunctionAddress = Address(0x14002db80);
-	*func_ptr = TargetFunctionAddress;
-
-	DetourAttach(func_ptr, &testInjection);
-}
